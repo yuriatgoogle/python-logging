a15325da869b5269bac1b1f460386926052062c9
Yuri Grinshteyn <ygrinshteyn@ygrinshteyn-macbookpro1.roam.corp.google.com>
2018-04-12 15:19:44 -0700

    updating with random logging

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
 from app import app
+import logging
+import random
+import string
 
 @app.route('/')
 @app.route('/index')
 def index():
-    // TODO - add logging
+    # print random string to log
+    randomString = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
+    logging.warn("random string - " + randomString)
     return "Hello, World!"
