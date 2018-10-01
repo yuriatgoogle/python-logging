243c14adddb8e1aac987e7b0e41cf23b87157ab2
Yuri Grinshteyn <ygrinshteyn@ygrinshteyn-macbookpro1.roam.corp.google.com>
2018-10-01 12:14:02 -0700

    using new Stackdriver logging agents

--- app.py
+++ app.py
@@ -6,23 +6,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    # generate random string to log
-    randomString = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
+    # generate random 10-digit number to log
+    randomString = ''.join([random.choice(string.digits) for n in xrange(10)])
     
     #randomly pick a logging level
     logLevel = random.randint(1,3)
-    logging.info("log level is " + str(logLevel))
+    # logging.error("output value:" + randomString)
     
     # log based on level
     if (logLevel==1): 
-        logging.error("random string:" + randomString)
+        logging.error("myError:" + randomString)
+        return "myError:" + randomString
     elif (logLevel==2): 
-        logging.warn("random string:" + randomString)
+        logging.warn("myWarning:" + randomString)
+        return "myWarning:" + randomString
     elif (logLevel==3):
-        logging.debug("random string:" + randomString)
+        logging.debug("myDebugMessage:" + randomString)
+        return "myDebug:" + randomString
     else:
         logging.fatal("problem")
-    return 'Flask Dockerized'
+        return "fatalError"
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
